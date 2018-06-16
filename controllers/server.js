0c37957f18106d07466b8073c845abc613f889fd
oconnelljustin <justin@cryptographicasset.com>
2018-06-16 09:22:04 -0700

    homework

---
+++ controllers/server.js
@@ -0,0 +1,33 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js')
+
+var request = require('request');
+var cheerio = require('cheerio');
+mongoose.Promise = Promise;
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static("public"));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
